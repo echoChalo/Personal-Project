9a31844020705e23ffbd5e70848cf99481db542e
echoChalo <1539555612@qq.com>
2024-06-10 20:51:33 +0800

    upload JAVA project

---
+++ JAVA课程大作业/MySQL表格/system_admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO `system_admin` (`id`, `username`, `sysname`, `password`, `gender`, `telephone`) VALUES (1, 'admin', '体适能', '123123', '男', '10086');
+INSERT INTO `system_admin` (`id`, `username`, `sysname`, `password`, `gender`, `telephone`) VALUES (2, 'cxk', '鸡你太美', '114514', '女', '114514');
+INSERT INTO `system_admin` (`id`, `username`, `sysname`, `password`, `gender`, `telephone`) VALUES (3, 'cxk2', '牙签', '1', '男', '110');
+INSERT INTO `system_admin` (`id`, `username`, `sysname`, `password`, `gender`, `telephone`) VALUES (5, 'wyf', '无牙签', '114514', '男', '114514');
+INSERT INTO `system_admin` (`id`, `username`, `sysname`, `password`, `gender`, `telephone`) VALUES (6, 'homo', '田所浩二114', '114514', '男', '114514');
